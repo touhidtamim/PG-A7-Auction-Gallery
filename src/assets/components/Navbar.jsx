54a5312932193c39a895508645e385f932594a78
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-07 11:23:07 +0600

    updated navbar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 const Navbar = () => {
   return (
@@ -14,20 +14,41 @@ const Navbar = () => {
               <span className="text-yellow-500">Gallery</span>
             </div>
           </div>
+
           {/* middle buttons */}
           <div className="navbar-center flex">
             <ul className="menu menu-horizontal px-1 gap-6">
-              <li><a className="font-semibold text-gray-900 hover:text-gray-900">Home</a></li>
-              <li><a className="font-semibold text-gray-900 hover:text-gray-900">Auctions</a></li>
-              <li><a className="font-semibold text-gray-900 hover:text-gray-900">Categories</a></li>
-              <li><a className="font-semibold text-gray-900 hover:text-gray-900">How to works</a></li>
+              <li>
+                <a className="font-semibold text-gray-900 hover:text-gray-900">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="font-semibold text-gray-900 hover:text-gray-900">
+                  Auctions
+                </a>
+              </li>
+              <li>
+                <a className="font-semibold text-gray-900 hover:text-gray-900">
+                  Categories
+                </a>
+              </li>
+              <li>
+                <a className="font-semibold text-gray-900 hover:text-gray-900">
+                  How to works
+                </a>
+              </li>
             </ul>
           </div>
+
           {/* Right part */}
           <div className="navbar-end">
             <div className="flex gap-3 items-center">
               <button className="btn btn-ghost btn-circle">
-                <FontAwesomeIcon icon={faBell} className="text-lg text-gray-600" />
+                <FontAwesomeIcon
+                  icon={faBell}
+                  className="text-lg text-gray-600"
+                />
               </button>
               <div className="avatar">
                 <div className="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
